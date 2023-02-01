4a783216e6079a4bccbe43d86c591c2edc39becf
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2023-02-01 18:04:22 +0700

    push

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const arr = [1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 13, 12, 14, 15, 16, 17, 18, 19, 20]
 
+const quarto = Math.floor(arr.length / 4)
+const half = arr.length / 2
+
 for (let i = 0; i < arr.length; i++) {
-    if (i < Math.floor(arr.length / 4)) {
-        console.log(`${arr[i]} -> ${arr[arr.length / 2 - i -1]} -> ${arr[i]}`)
-    } else if (i > Math.floor(arr.length / 4 - i) && i < arr.length / 2) {
-        console.log(`${arr[Math.floor(arr.length / 4 ) + i]} -> ${arr[Math.floor(arr.length / 4) + arr.length - i - 1]} -> ${arr[Math.floor(arr.length / 4) + arr.length / 2 - i - 1]}`)
+    if (i < quarto) {
+        console.log(`${arr[i]} -> ${arr[half - i -1]} -> ${arr[i]}`)
+    } else if (i > quarto - i && i < half) {
+        console.log(`${arr[quarto + i]} -> ${arr[quarto + arr.length - i - 1]} -> ${arr[quarto + half - i - 1]}`)
     }
 }

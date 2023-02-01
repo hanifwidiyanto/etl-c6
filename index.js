678b8039eb3c85281d0216f9f0688eb411e4a14a
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2023-02-01 18:05:22 +0700

    push

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const arr = [1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 13, 12, 14, 15, 16, 17, 18, 19, 
 
 const quarto = Math.floor(arr.length / 4)
 const half = arr.length / 2
+const poin = 1
 
 for (let i = 0; i < arr.length; i++) {
     if (i < quarto) {
-        console.log(`${arr[i]} -> ${arr[half - i -1]} -> ${arr[i]}`)
+        console.log(`${arr[i]} -> ${arr[half - i - poin]} -> ${arr[i]}`)
     } else if (i > quarto - i && i < half) {
-        console.log(`${arr[quarto + i]} -> ${arr[quarto + arr.length - i - 1]} -> ${arr[quarto + half - i - 1]}`)
+        console.log(`${arr[quarto + i]} -> ${arr[quarto + arr.length - i - poin]} -> ${arr[quarto + half - i - poin]}`)
     }
 }
